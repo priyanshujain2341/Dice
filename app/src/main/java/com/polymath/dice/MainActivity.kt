7a2b86c87a680c8e23cae4c59d8c8c063f387727
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-07 13:42:08 +0530

    Added a toast message and update TextView on Button-Click

--- app/src/main/java/com/polymath/dice/MainActivity.kt
+++ app/src/main/java/com/polymath/dice/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.polymath.dice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button);
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "The Dice is rolled!", Toast.LENGTH_SHORT).show();
+            val resultTextView: TextView = findViewById(R.id.textView);
+            resultTextView.text = "6"
+        }
     }
 }
