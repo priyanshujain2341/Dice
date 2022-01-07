a4fb951b4aed50d878b2f7e649dcf6558579c0a9
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-07 13:58:49 +0530

    Reformat code

--- app/src/main/java/com/polymath/dice/MainActivity.kt
+++ app/src/main/java/com/polymath/dice/MainActivity.kt
@@ -6,10 +6,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
@@ -19,19 +17,25 @@ class MainActivity : AppCompatActivity()
         }
     }
 
-    private fun rollDice()
-    {
+    /**
+     * Roll the dice and update the screen with a result
+     */
+    private fun rollDice() {
+        //Create a dice object
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        //Update the Screen with the dice Roll number
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice(private val numSides: Int)
-{
-    fun roll():Int
-    {
+/**
+ * This class is the implementation of a dice
+ */
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
