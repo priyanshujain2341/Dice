c5e073145e45e8161e1423352070bb616ab1cdcb
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-07 13:54:55 +0530

    Added the dice roll logic

--- app/src/main/java/com/polymath/dice/MainActivity.kt
+++ app/src/main/java/com/polymath/dice/MainActivity.kt
@@ -6,15 +6,32 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button);
+        val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "The Dice is rolled!", Toast.LENGTH_SHORT).show();
-            val resultTextView: TextView = findViewById(R.id.textView);
-            resultTextView.text = "6"
+            Toast.makeText(this, "The Dice is rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice()
+    {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int)
+{
+    fun roll():Int
+    {
+        return (1..numSides).random()
     }
 }
